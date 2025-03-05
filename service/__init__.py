7606f060331d1ca44d34187b268f35ec2616915c
pavkuchin <45168975+PaKuchin@users.noreply.github.com>
2025-03-05 21:00:33 +0200

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -6,25 +6,27 @@ and SQL database
 """
 import sys
 from flask import Flask
-from flask_talisman import Talisman
 from service import config
 from service.common import log_handlers
+from flask_talisman import Talisman
+from flask_cors import CORS
 
 # Create Flask application
 app = Flask(__name__)
-app.config.from_object(config)
-
-# Initialize security headers (HTTPS, CORS, etc.)
 talisman = Talisman(app)
+cors = CORS(app)
+app.config.from_object(config)
 
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
 from service import routes, models  # noqa: F401 E402
+
 # pylint: disable=wrong-import-position
 from service.common import error_handlers, cli_commands  # noqa: F401 E402
 
 # Set up logging for production
 log_handlers.init_logging(app, "gunicorn.error")
+
 app.logger.info(70 * "*")
 app.logger.info("  A C C O U N T   S E R V I C E   R U N N I N G  ".center(70, "*"))
 app.logger.info(70 * "*")
